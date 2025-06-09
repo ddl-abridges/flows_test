9c6be8dcd3d89c2c28e5c609ee43cba84f68daeb
ddl-abridges <96255312+ddl-abridges@users.noreply.github.com>
2025-06-09 13:55:58 -0700

    Create data_csv.py

---
+++ data_csv.py
@@ -0,0 +1,32 @@
+from flytekit import workflow
+from flytekit.types.file import FlyteFile
+from typing import TypeVar, NamedTuple
+from flytekitplugins.domino.helpers import Input, Output, run_domino_job_task
+from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask, GitRef, EnvironmentRevisionSpecification, EnvironmentRevisionType, DatasetSnapshot
+
+# Set the name of this variable to the name of your Domino's standard environment
+environment_name="Domino Standard Environment Py3.10 R4.4"
+
+# Set if you want caching on or off. 
+cache=False
+
+@workflow
+def retrieve_data(data_path_a: str, data_path_b: str): 
+    '''
+
+    pyflyte run --remote data_csv.py model_training --data_path_a /mnt/datasetA.csv
+    '''
+
+    task1 = run_domino_job_task(
+        flyte_task_name='Retrieve Audit ',
+        command='python load-data-A.py',
+        inputs=[Input(name='data_path', type=str, value=data_path_a)],
+        output_specs=[Output(name='datasetA', type=FlyteFile[TypeVar('csv')])],
+        use_project_defaults_for_omitted=True,
+        environment_name=environment_name,
+        hardware_tier_name="Small",
+        cache=cache,
+        cache_version="1.0"
+    )
+
+    return 
